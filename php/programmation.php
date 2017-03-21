e2abc887dc95e4bd558be4840bebe05c0aef72bc
BlasonB <pcdotwild@gmail.com>
2017-03-21 10:25:29 +0100

    ajout fichier programmation.php

---
+++ php/programmation.php
@@ -0,0 +1,21 @@
+<?php
+require("header.php");
+?>
+
+    <div class="row">
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+    </div>
+<?php
+require("header.php");
+?>
