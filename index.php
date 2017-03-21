4eee6851e9c0284b8b0c0896e3834b725c51f040
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-03-21 17:27:54 +0100

    Création de la page d'accueil version maquette

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilder1
- * Date: 21/03/17
- * Time: 10:05
- */
+require('php/header.php');
+?>
+<div class="text-center">
+    <img src="src/img/logo-only.png" alt="Logo du Festival" class="welcome_logo">
+</div>
+<div class="container-fluid">
+    <div class="row welcome_navRow">
+        <button type="button" class="btn btn-default col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1 welcome_navButton">PROGRAMMATION</button>
+        <button type="button" class="btn btn-default col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-2 welcome_navButton">BILLETERIE</button>
+        <button type="button" class="btn btn-default col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1 welcome_navButton">CASHLESS</button>
+        <button type="button" class="btn btn-default col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-2 welcome_navButton">LA VIE AU FESTIVAL</button>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-xs-offset-0 col-sm-5 col-sm-offset-1">
+            <ul class="welcome_newsUl">
+                <li class="welcome_newsLi">
+                    <h1 class="welcome_newsH1">Titre</h1>
+                    <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+                </li>
+                <li class="welcome_newsLi">
+                    <h1 class="welcome_newsH1">Titre</h1>
+                    <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+                </li>
+                <li class="welcome_newsLi">
+                    <h1 class="welcome_newsH1">Titre</h1>
+                    <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+                </li>
+            </ul>
+        </div>
+        <div class="col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1 hidden-xs">
+            <iframe src="https://embed.spotify.com/?uri=spotify:track:5JunxkcjfCYcY7xJ29tLai" frameborder="0" allowtransparency="true" width="300" height="380"></iframe>
+        </div>
+    </div>
+</div>
+<?php
+require('php/footer.php');
+?>
