5df87b4c52ec7f36effa5ddf41961cda75d1d4de
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-03-22 17:38:43 +0100

    refonte de la page d'accueil

--- index.php
+++ index.php
@@ -1,33 +1,68 @@
 <?php
 require('php/header.php');
 ?>
-<div class="text-center">
-    <img src="src/img/logo-only.png" alt="Logo du Festival" class="welcome_logo hidden-xs">
-    <img src="src/img/logo-only.png" alt="Logo du Festival" class="welcome_logo img-responsive visible-xs">
-</div>
-<div class="container-fluid">
-    <div class="row welcome_navRow">
+    <div class="text-center">
+        <img src="src/img/logo-only.png" alt="Logo du Festival" class="welcome_logo hidden-xs">
+        <img src="src/img/logo-only.png" alt="Logo du Festival" class="welcome_logo img-responsive visible-xs">
     </div>
-    <div class="row">
-        <!--
-        <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 welcome_newsDiv text-center">
-            <img src="http://placehold.it/200x200">
-            <h1 class="welcome_newsH1">Titre</h1>
-            <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
-        </div>
-        <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_newsDiv text-center">
-            <img src="http://placehold.it/200x200">
-            <h1 class="welcome_newsH1">Titre</h1>
-            <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
-        </div>
-        <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_newsDiv text-center">
-            <img src="http://placehold.it/200x200">
-            <h1 class="welcome_newsH1">Titre</h1>
-            <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
-        </div>
-        -->
+    <br />
+    <br />
+    <div class="welcome_divTitle text-center">
+        <h1 class="welcome_divH1">PROGRAMMATION</h1>
     </div>
-</div>
+    <div class="welcome_div">
+        <a href="#" class="welcome_divImg"><img src="src/img/programmation.png" class="img-responsive"></a>
+    </div>
+    <div class="welcome_divTitle text-center">
+        <h1 class="welcome_divH1">BILLETERIE</h1>
+    </div>
+    <div class="welcome_div">
+        <div class="row">
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 welcome_ticketDiv text-center">
+                <img src="http://placehold.it/200x200">
+                <h1 class="welcome_ticketH1">Titre</h1>
+                <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            </div>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_ticketDiv text-center">
+                <img src="http://placehold.it/200x200">
+                <h1 class="welcome_ticketH1">Titre</h1>
+                <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            </div>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_ticketDiv text-center">
+                <img src="http://placehold.it/200x200">
+                <h1 class="welcome_ticketH1">Titre</h1>
+                <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            </div>
+        </div>
+    </div>
+    <div class="welcome_divTitle text-center">
+        <h1 class="welcome_divH1">ACTUALITES</h1>
+    </div>
+    <br />
+    <br />
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 welcome_newsDiv text-left">
+                <img src="http://placehold.it/200x200">
+                <h1 class="welcome_newsH1">Titre</h1>
+                <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            </div>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_newsDiv text-center">
+                <img src="http://placehold.it/200x200">
+                <h1 class="welcome_newsH1">Titre</h1>
+                <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            </div>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_newsDiv text-right">
+                <img src="http://placehold.it/200x200">
+                <h1 class="welcome_newsH1">Titre</h1>
+                <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            </div>
+        </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
 <?php
 require('php/footer.php');
 ?>
