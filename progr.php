bd5e332434afbac2c0412e5f4ae53a74228ff153
BlasonB <benedite.j@gmail.com>
2017-03-27 15:33:30 +0200

    pas de changements

--- progr.php
+++ progr.php
@@ -1,50 +1,48 @@
 <?php
-require ('php/header.php');
+require('php/header.php');
 ?>
 
-<div class="container-fluid">
+    <div class="container-fluid">
 
         <div class="row">
             <div class="col-md-3">
-                <img src="src/img/chuck.png">
-                <div class="">
+                <div class="item1">
+                    <img src="src/img/chuck.png">
+                    <div class="">
+                    </div>
 
                 </div>
             </div>
             <div class="col-md-3">
-                <img src="src/img/chuck.png">
-                <div class="">
+                <div class="item2">
+                    <img src="src/img/chuck.png">
+                    <div class="">
+                    </div>
 
                 </div>
             </div>
             <div class="col-md-3">
-                <img src="src/img/chuck.png">
-                <div class="">
+                <div class="item3">
+                    <img src="src/img/chuck.png">
+                    <div class="">
+                    </div>
 
                 </div>
             </div>
             <div class="col-md-3">
-                <img src="src/img/chuck.png">
-                <div class="">
+                <div class="item4">
+                    <img src="src/img/chuck.png">
+                    <div class="">
+                    </div>
 
                 </div>
             </div>
+
 
         </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
+    </div>
 
 
 <?php
-require ('php/footer.php');
+require('php/footer.php');
 ?>
