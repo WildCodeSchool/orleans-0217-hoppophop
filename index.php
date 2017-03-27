819e46b2b6ee90b77bf5a2e8e1957a6f7ca979c1
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-03-27 14:06:44 +0200

    Module de news ++, polices d'écritures

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require('php/header.php');
     </div>
     <br />
     <br />
-    <div class="welcome_divTitle text-center">
+    <div class="welcome_divTitle text-center hidden-xs">
         <h1 class="welcome_divH1">PROGRAMMATION</h1>
     </div>
-    <div class="welcome_div">
+    <div class="welcome_div hidden-xs">
         <a href="#" class="welcome_divImg"><img src="src/img/programmation.png" class="img-responsive"></a>
     </div>
     <div class="welcome_divTitle text-center">
@@ -20,17 +20,7 @@ require('php/header.php');
         <div class="row">
             <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 welcome_ticketDiv text-center">
                 <img src="http://placehold.it/200x200">
-                <h1 class="welcome_ticketH1">Titre</h1>
-                <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
-            </div>
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_ticketDiv text-center">
-                <img src="http://placehold.it/200x200">
-                <h1 class="welcome_ticketH1">Titre</h1>
-                <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
-            </div>
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_ticketDiv text-center">
-                <img src="http://placehold.it/200x200">
-                <h1 class="welcome_ticketH1">Titre</h1>
+                <h2 class="welcome_ticketH1">Titre</h2>
                 <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
             </div>
         </div>
@@ -42,22 +32,27 @@ require('php/header.php');
     <br />
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 welcome_newsDiv text-left">
-                <img src="http://placehold.it/200x200">
-                <h1 class="welcome_newsH1">Titre</h1>
-                <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2">
+                <div class="welcome_newsDiv text-left">
+                    <img src="src/img/la_jungle.jpg" class="img-responsive">
+                    <h2 class="welcome_newsH1">La Jungle</h2>
+                    <p class="welcome_newsP">Une six-cordes et un casio. Il n’en faut parfois pas plus pour faire péter le mercure et irriter les yeux de sueur après deux breaks et trois accords. Deux singes rouquins, un très grand et un plus petit, vous emmènent dans leur milieu naturel : La Jungle !</p>
+                </div>
             </div>
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_newsDiv text-left">
-                <img src="http://placehold.it/200x200">
-                <h1 class="welcome_newsH1">Titre</h1>
-                <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1">
+                <div class="welcome_newsDiv text-left">
+                    <img src="src/img/tristesse_contemporaine.jpg" class="img-responsive">
+                    <h2 class="welcome_newsH1">Tristesse Contemporaine</h2>
+                    <p class="welcome_newsP">Tristesse Contemporaine. Acte III, scène 1. Après un premier album en forme de déclaration d’indépendance Tristesse Contemporaine et un second en lévitation au dessus de la meute Stay Golden, Stop and Start vient clore une trilogie tout en anticipant un nouveau cycle.</p>
+                </div>
             </div>
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1 welcome_newsDiv text-left">
-                <img src="http://placehold.it/200x200">
-                <h1 class="welcome_newsH1">Titre</h1>
-                <p class="welcome_newsP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1">
+                <div class="welcome_newsDiv text-left">
+                    <img src="src/img/tristesse_contemporaine.jpg" class="img-responsive">
+                    <h2 class="welcome_newsH1">Tristesse Contemporaine</h2>
+                    <p class="welcome_newsP">Tristesse Contemporaine. Acte III, scène 1. Après un premier album en forme de déclaration d’indépendance Tristesse Contemporaine et un second en lévitation au dessus de la meute Stay Golden, Stop and Start vient clore une trilogie tout en anticipant un nouveau cycle.</p>
+                </div>
             </div>
-
         </div>
     </div>
     <br />
