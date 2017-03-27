0fc2b562d95362ecd32efe40a67b165f1e3966ba
BlasonB <benedite.j@gmail.com>
2017-03-27 15:15:22 +0200

    ajouts du fichier page programation, à ignorer

---
+++ progr.php
@@ -0,0 +1,50 @@
+<?php
+require ('php/header.php');
+?>
+
+<div class="container-fluid">
+
+        <div class="row">
+            <div class="col-md-3">
+                <img src="src/img/chuck.png">
+                <div class="">
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <img src="src/img/chuck.png">
+                <div class="">
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <img src="src/img/chuck.png">
+                <div class="">
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <img src="src/img/chuck.png">
+                <div class="">
+
+                </div>
+            </div>
+
+        </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+require ('php/footer.php');
+?>
