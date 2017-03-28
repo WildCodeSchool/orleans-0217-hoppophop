181dd0c83a8d01dd4778501987917a0bac58496a
BlasonB <benedite.j@gmail.com>
2017-03-28 17:52:10 +0200

    modifs page programmation, tel que l'ajout du header(loin d'être définitif)

--- progr.php
+++ progr.php
@@ -2,37 +2,43 @@
 require('php/header.php');
 ?>
 
-    <div class="container-fluid">
+<div class="container-fluid headerpro">
+</div>
+
+
+<div class="container-fluid">
+
+    <div class="container">
 
         <div class="row">
 
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
-                    <div class="labelx text-center"><p>Chuck</p></div>
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
-                <div class="labelx text-center"><p>Chuck</p></div>
+
             </div>
 
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
-                <div class="labelx">Chuck</div>
             </div>
 
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
-                <div class="labelx">Chuck</div>
             </div>
 
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p</div>
                 </div>
-                <div class="labelx">Chuck</div>
             </div>
 
         </div>
@@ -41,27 +47,33 @@ require('php/header.php');
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="thumbnail itemx">
                     <img src="src/img/chuck.png">
+                    <div class="labelx text-center"><p class="layoutartiste">Chuck</p></div>
                 </div>
             </div>
 
         </div>
+
     </div>
 
+</div>
 
 <?php
 require('php/footer.php');
