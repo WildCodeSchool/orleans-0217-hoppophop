d23133771cb46e6ac79901ba05ed84a322175201
BlasonB <benedite.j@gmail.com>
2017-03-28 14:15:00 +0200

    modifications page programmation

--- progr.php
+++ progr.php
@@ -5,28 +5,32 @@ require('php/header.php');
     <div class="container-fluid">
 
         <div class="row">
+
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                     <div class="labelx text-center"><p>Chuck</p></div>
                 </div>
                 <div class="labelx text-center"><p>Chuck</p></div>
             </div>
+
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
                 <div class="labelx">Chuck</div>
             </div>
+
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
                 <div class="labelx">Chuck</div>
             </div>
+
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
                 <div class="labelx">Chuck</div>
             </div>
@@ -36,22 +40,22 @@ require('php/header.php');
         <div class="row">
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="thumbnail itemx">
-                    <img src="../src/img/chuck.png">
+                    <img src="src/img/chuck.png">
                 </div>
             </div>
 
