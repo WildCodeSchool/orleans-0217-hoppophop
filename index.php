05548588b7e6859861526f0eb993342ecc9dbd8b
M13K <ag.koffi@hotmail.fr>
2017-03-30 11:26:24 +0200

    push branch

--- index.php
+++ index.php
@@ -5,15 +5,12 @@ require('php/header.php');
 
 
 <div class="container-fluid">
-<<<<<<< HEAD
+
     <div class="row welcome_navRow">
 
 
 
-=======
 
-
->>>>>>> artiste
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-3">
