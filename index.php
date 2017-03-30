880a7b146be353476af1157b190cadca07daf691
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-03-30 17:37:31 +0200

    Feuille de style

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require('php/header.php');
         <div class="row">
             <div class="col-xs-3">
                 <a href="#">
-                    <img src="src/img/button-left.png" alt="Boutton Gauche" class="img-responsive welcome_buttonLeft">
+                    <img src="src/img/button-left.png" alt="Boutton Gauche" class="img-responsive logo-buttonLeft">
                 </a>
             </div>
             <div class="col-xs-6">
@@ -13,8 +13,8 @@ require('php/header.php');
             </div>
             <div class="col-xs-3">
                 <div class="">
-                    <a href="#">
-                        <img src="src/img/button-right.png" alt="Boutton Droite" class="img-responsive welcome_buttonRight">
+                    <a href="php/programmation.php">
+                        <img src="src/img/button-right.png" alt="Boutton Droite" class="img-responsive logo-buttonRight">
                     </a>
                 </div>
             </div>
@@ -22,55 +22,45 @@ require('php/header.php');
     </div>
     <br />
     <br />
-    <div class="welcome_divTitle text-center visible-sm">
-        <h1 class="welcome_divH1">PROGRAMMATION</h1>
+    <div class="separator-titleDiv text-center visible-sm">
+        <h1 class="separator-title font-dokyo">PROGRAMMATION</h1>
     </div>
-    <div class="welcome_div visible-sm">
-        <a href="#" class="welcome_divImg"><img src="src/img/programmation.png" class="img-responsive"></a>
+    <div class="separator-div visible-sm">
+        <a href="#" class="separator-content"><img src="src/img/programmation.png" class="img-responsive"></a>
     </div>
-    <div class="welcome_divTitle text-center visible-sm">
-        <h1 class="welcome_divH1">BILLETERIE</h1>
+    <div class="separator-titleDiv text-center visible-sm">
+        <h1 class="separator-title font-dokyo">BILLETERIE</h1>
     </div>
-    <div class="welcome_div visible-sm">
+    <div class="separator-div visible-sm">
         <div class="row">
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 welcome_ticketDiv text-center">
+            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2 separator-content text-center">
                 <img src="http://placehold.it/200x200">
-                <h2 class="welcome_ticketH1">Titre</h2>
-                <p class="welcome_ticketP">Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
+                <h2>Titre</h2>
+                <p>Principium autem unde latius se funditabat, emersit ex negotio tali. Chilo ex vicario et coniux eius Maxima nomine, questi apud Olybrium ea tempestate urbi praefectum, vitamque suam venenis petitam adseverantes inpetrarunt ut hi, quos suspectati sunt, ilico rapti conpingerentur in vincula, organarius Sericus et Asbolius palaestrita et aruspex Campensis.</p>
             </div>
         </div>
     </div>
-    <div class="welcome_divTitle text-center">
-        <h1 class="welcome_divH1">ACTUALITES</h1>
+    <div class="separator-titleDiv text-center">
+        <h1 class="separator-title font-dokyo">ACTUALITES</h1>
     </div>
     <br />
     <br />
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-2">
-                <div class="welcome_newsDiv text-left">
-                    <img src="src/img/la_jungle.jpg" class="img-responsive">
-                    <h2 class="welcome_newsH1">La Jungle</h2>
-                    <hr class="welcome_newsHR">
-                    <p class="welcome_newsP">Une six-cordes et un casio. Il n’en faut parfois pas plus pour faire péter le mercure et irriter les yeux de sueur après deux breaks et trois accords. Deux singes rouquins, un très grand et un plus petit, vous emmènent dans leur milieu naturel : La Jungle !</p>
+            <?php
+            for($i=0;$i<3;$i++){
+                ?>
+                <div class="col-xs-12 col-xs-offset-0 col-sm-4">
+                    <div class="content-div text-left">
+                        <img src="src/img/la_jungle.jpg" class="img-responsive">
+                        <h2 class="news-title font-circula">La Jungle</h2>
+                        <hr class="hr">
+                        <p>Une six-cordes et un casio. Il n’en faut parfois pas plus pour faire péter le mercure et irriter les yeux de sueur après deux breaks et trois accords. Deux singes rouquins, un très grand et un plus petit, vous emmènent dans leur milieu naturel : La Jungle !</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1">
-                <div class="welcome_newsDiv text-left">
-                    <img src="src/img/tristesse_contemporaine.jpg" class="img-responsive">
-                    <h2 class="welcome_newsH1">Tristesse Contemporaine</h2>
-                    <hr class="welcome_newsHR">
-                    <p class="welcome_newsP">Tristesse Contemporaine. Acte III, scène 1. Après un premier album en forme de déclaration d’indépendance Tristesse Contemporaine et un second en lévitation au dessus de la meute Stay Golden, Stop and Start vient clore une trilogie.</p>
-                </div>
-            </div>
-            <div class="col-xs-12 col-xs-offset-0 col-sm-2 col-sm-offset-1">
-                <div class="welcome_newsDiv text-left">
-                    <img src="src/img/tristesse_contemporaine.jpg" class="img-responsive">
-                    <h2 class="welcome_newsH1">Tristesse Contemporaine</h2>
-                    <hr class="welcome_newsHR">
-                    <p class="welcome_newsP">Tristesse Contemporaine. Acte III, scène 1. Après un premier album en forme de déclaration d’indépendance Tristesse Contemporaine et un second en lévitation au dessus de la meute Stay Golden, Stop and Start vient clore une trilogie.</p>
-                </div>
-            </div>
+                <?php
+            }
+            ?>
         </div>
     </div>
     <br />
---
+++ php/artiste.php
@@ -0,0 +1,91 @@
+<?php
+include("header.php");
+
+?>
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+    <script> AOS.init(); </script>
+    <div class="container-fluid hidden-xs">
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-1">
+                <div class="artiste-info text-left">
+                    <h1 class="artiste-title">ARTISTE</h1>
+                    <p>Hinc ille commotus ut iniusta perferens et indigna praefecti custodiam protectoribus mandaverat fidis
+                        . quo conperto Montius tunc quaestor acer quidem sed ad lenitatem propensior,
+                        consulens in commune advocatos palatinarum primos scholarum adlocutus est mollius docens</p>
+                    <hr class="hr">
+                    <div class="artiste-tag">
+                        <h3>France/Chanson/Chanson</h3>
+                    </div>
+                    <div class="artiste-social">
+                        <img src="../src/img/social/fb-logo.png" class="social-logo">
+                        <img src="../src/img/social/twitter-logo.png" class="social-logo">
+                        <img src="../src/img/social/instagram-logo.png" class="social-logo">
+                        <img src="../src/img/social/youtube-logo.png" class="social-logo">
+                        <img src="../src/img/social/spotify-logo.png" class="social-logo">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4 col-sm-4">
+                <div data-aos="fade-left"><img src="http://www.auplod.com/u/oadupl94867.jpeg" class="img-responsive artiste-img" ></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-1 col-sm-6">
+                <div class="artiste-vid">
+                    <iframe width="100%" height="480" src="https://www.youtube.com/embed/wHylQRVN2Qs" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-xs-4 col-sm-4">
+                <div class="artiste-prog font-dokyo">
+                    <h2>VEN. 16 SEPTEMBRE</h2>
+                    <hr class="hr">
+                    <p>JARDIN DE L'EVÊCHÉ<br/>
+                    21H35 > 22H35</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid visible-xs clearfix">
+        <div class="row">
+            <div class="col-xs-12">
+                <img src="http://www.auplod.com/u/oadupl94867.jpeg" class="img-responsive">
+            </div>
+            <div class="col-xs-12" >
+                <div class="artiste-info text-center">
+                    <h1>ARTISTE</h1>
+                    <p>Hinc ille commotus ut iniusta perferens et indigna praefecti custodiam protectoribus mandaverat fidis
+                        . quo conperto Montius tunc quaestor acer quidem sed ad lenitatem propensior,
+                        consulens in commune advocatos palatinarum primos scholarum adlocutus est mollius docens</p>
+                    <hr class="hr">
+                    <div class="artiste-progMobile font-dokyo">
+                        <p>VEN. 16 SEPTEMBRE<br/></p>
+                        <hr class="hr">
+                        <p>JARDIN DE L'EVÊCHÉ<br/>
+                            21H35 > 22H35</p>
+                    </div>
+                    <h3>France/Chanson/Chanson</h3>
+                    <div class="artiste-social">
+                        <img src="../src/img/social/fb-logo.png" class="social-logoMobile">
+                        <img src="../src/img/social/twitter-logo.png" class="social-logoMobile">
+                        <img src="../src/img/social/instagram-logo.png" class="social-logoMobile">
+                        <img src="../src/img/social/youtube-logo.png" class="social-logoMobile">
+                        <img src="../src/img/social/spotify-logo.png" class="social-logoMobile">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <div class="artiste-vidMobile"></div>
+                <iframe width="100%" height="280" src="https://www.youtube.com/embed/wHylQRVN2Qs" frameborder="0" allowfullscreen></iframe>
+            </div>
+
+        </div>
+    </div>
+
+
+    </div>
+
+
+<?php
+require('footer.php');
+?>
