f641fb2dc1002071b0cf5f9f4b9d6c4a77208e03
BlasonB <benedite.j@gmail.com>
2017-03-31 11:36:35 +0200

    ajouts des fichiers contact

--- php/contact.php
+++ php/contact.php
@@ -3,32 +3,33 @@ require('../php/header.php');
 ?>
 
 
-<div class="container text-center">
-<section id="hire">
-    <h1>Contact Me</h1>
+<div class="container-fluid text-center contact-content">
+        <section id="hire">
+            <h1>Contact Me</h1>
 
-    <form>
-        <div class="field name-box">
-            <input type="text" id="name" placeholder="Who Are You?"/>
-            <label for="name">Name</label>
-            <span class="ss-icon">check</span>
-        </div>
+            <form>
+                <div class="field name-box">
+                    <input type="text" id="name" placeholder="Who Are You?"/>
+                    <label for="name">Name</label>
+                    <span class="ss-icon">check</span>
+                </div>
 
-        <div class="field email-box">
-            <input type="text" id="email" placeholder="name@email.com"/>
-            <label for="email">Email</label>
-            <span class="ss-icon">check</span>
-        </div>
+                <div class="field email-box">
+                    <input type="text" id="email" placeholder="name@email.com"/>
+                    <label for="email">Email</label>
+                    <span class="ss-icon">check</span>
+                </div>
 
-        <div class="field msg-box">
-            <textarea id="msg" rows="4" placeholder="Your message goes here..."></textarea>
-            <label for="msg">Msg</label>
-            <span class="ss-icon">check</span>
-        </div>
+                <div class="field msg-box">
+                    <textarea id="msg" rows="4" placeholder="Your message goes here..."></textarea>
+                    <label for="msg">Msg</label>
+                    <span class="ss-icon">check</span>
+                </div>
 
-        <input class="button" type="submit" value="Send" />
-    </form>
-</section>
+                <input class="button" type="submit" value="Send" />
+            </form>
+        </section>
+
 </div>
 
 
