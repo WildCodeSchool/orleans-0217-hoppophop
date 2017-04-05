eec156242deb508b7b1acd1a4bdc2d5235886daf
M13K <ag.koffi@hotmail.fr>
2017-04-05 12:11:02 +0200

    update partenaire

--- php/partner.php
+++ php/partner.php
@@ -4,15 +4,16 @@ include('header.php');
 
     <div class="container-fluid">
         <div class="header-div">
-            <h1 class="partenaire">NOS PARTENAIRES</h1>
+            <h1 class="partenaire_title">NOS PARTENAIRES</h1>
         </div>
         <div class="bloc-page">
-            <div class="row">
+            <div class="row text-center">
                 <div class="separator-titleDiv separator-green">
                 <h2 class="partenaire-off">PARTENAIRES OFFICIELS</h2>
+                    <hr class="separator-partenaire">
                 </div>
-                <div class="col-xs-3 col-md-3">
-                    <a href="#" class="">
+                <div class="col-xs-3 col-md-4">
+                    <a href="#" class="adami_sponsor">
                         <img src="../src/img/sponsors/adami.png" class="img-responsive footer-sponsor">
                     </a>
                 </div>
@@ -57,6 +58,8 @@ include('header.php');
             <div class="row">
                 <div class="separator-titleDiv separator-green">
                 <h2 class="partenaire_off">PARTENAIRES PRIVES</h2>
+                    <hr class="separator-partenaire">
+
                 </div>
                 <div class="col-xs-3 col-md-3">
                     <a href="#" class="">
@@ -105,6 +108,8 @@ include('header.php');
             <div class="row">
                 <div class="separator-titleDiv separator-green">
                 <h2 class="partenaire_off">PARTENAIRES CULTURELS</h2>
+                    <hr class="separator-partenaire">
+
                 </div>
                 <div class="col-xs-3 col-md-3">
                     <a href="#" class="">
@@ -152,6 +157,8 @@ include('header.php');
             <div class="row">
                 <div class="separator-titleDiv separator-green">
                 <h2 class="partenaire_off">PARTENAIRES ASSOCIATIFS</h2>
+                    <hr class="separator-partenaire">
+
                 </div>
                 <div class="col-xs-3 col-md-3">
                     <a href="#" class="">
@@ -199,6 +206,8 @@ include('header.php');
             <div class="row">
                 <div class="separator-titleDiv separator-green">
                 <h2 class="partenaire_off">PARTENAIRES MEDIAS</h2>
+                    <hr class="separator-partenaire">
+
                 </div>
                 <div class="col-xs-3 col-md-3">
                     <a href="#" class="">
@@ -244,6 +253,11 @@ include('header.php');
                 </div>
             </div>
         </div>
+        <div class="partenaire_button">
+            <a href="../php/contact.php"><button class="btn btn-primary btn-lg">Devenez Partenaire</button></a>
+
+        </div>
+
     </div>
 
 
