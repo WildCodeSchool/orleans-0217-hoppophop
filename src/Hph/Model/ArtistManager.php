55b6a8dd0dcbcaad1e04774587b3ef2e9b778da0
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-04-11 17:56:29 +0200

    Prog git add .

---
+++ src/Hph/Model/ArtistManager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: quentin
+ * Date: 11/04/17
+ * Time: 17:05
+ */
+
+namespace HPH\Model;
+
+
+class ArtistManager extends \Hph\Db
+{
+    public function getArtists()
+    {
+        $req = "SELECT * FROM artist";
+        return $this->render($req, 'Artist');
+    }
+}
