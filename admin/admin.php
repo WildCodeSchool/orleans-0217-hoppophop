8da628bb1557f630c6789bbd768af4a1f7d6f583
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-04-26 11:08:47 +0200

    gestion des erreurs admin

--- admin/admin.php
+++ admin/admin.php
@@ -7,34 +7,31 @@ if (isset($_GET['page'])) {
 } else {
     $page = 'home';
 }
-if (!isset($_GET['error'])||$_GET['error']=='') {
+if (!isset($_GET['error'])) {
     $_GET['error'] = 1;
+}else if($_GET['error']==''){
+    $_GET['error'] = 12;
 }
-
-
 if ($page == 'home') {
     $home = new Hph\Controller\HomeController('back');
     $render = $home->render();
+}
 
-
-} else if ($page == 'artist') {
+else if ($page == 'artist') {
     $artist = new Hph\Controller\ProgrammationController('back');
     $render = $artist->listAllArtist();
 } else if ($page == 'addArtist') {
     $addArtist = new Hph\Model\ProgrammationManager();
-    if ($addArtist->addArtist($_POST, $_FILES)) {
-        header('Location: admin.php?page=artist');
-    }
+    $result = $addArtist->addArtist($_POST, $_FILES);
+    header('Location: admin.php?page=artist&error='.$result.'');
 } else if ($page == 'updateArtist') {
     $updateArtist = new Hph\Model\ProgrammationManager();
-    if ($updateArtist->updateArtist($_POST, $_FILES)) {
-        header('Location: admin.php?page=artist');
-    }
+    $result = $updateArtist->updateArtist($_POST, $_FILES);
+    header('Location: admin.php?page=artist&error='.$result.'');
 } else if ($page == 'deleteArtist') {
     $deleteArtist = new Hph\Model\ProgrammationManager();
-    if ($deleteArtist->deleteArtist($_POST['id'])) {
-        header('Location: admin.php?page=artist');
-    }
+    $result = $deleteArtist->deleteArtist($_POST['id']);
+    header('Location: admin.php?page=artist&error='.$result.'');
 }
 
 else if ($page == 'concert') {
@@ -42,111 +39,100 @@ else if ($page == 'concert') {
     $render = $concert->listConcert();
 } else if ($page == 'addConcert') {
     $addConcert = new Hph\Model\ConcertManager();
-    if ($addConcert->addConcert($_POST, $_FILES)) {
-        header('Location: admin.php?page=concert');
-    }
+    $result = $addConcert->addConcert($_POST);
+    header('Location: admin.php?page=concert&error='.$result.'');
 } else if ($page == 'updateConcert') {
     $updateConcert = new Hph\Model\ConcertManager();
-    if ($updateConcert->updateConcert($_POST, $_FILES)) {
-        header('Location: admin.php?page=concert');
-    }
+    $result = $updateConcert->updateConcert($_POST);
+    header('Location: admin.php?page=concert&error='.$result.'');
 } else if ($page == 'deleteConcert') {
     $deleteConcert = new Hph\Model\ConcertManager();
-    if ($deleteConcert->deleteConcert($_POST['id'])) {
-        header('Location: admin.php?page=concert');
-    }
+    $result = $deleteConcert->deleteConcert($_POST['id']);
+    header('Location: admin.php?page=concert&error='.$result.'');
 }
-
-
-
-
 
 else if ($page == 'place') {
     $place = new Hph\Controller\PlaceController('back');
     $render = $place->render();
 } else if ($page == 'addPlace') {
     $addPlace = new Hph\Model\PlaceManager();
-    if ($addPlace->addPlace($_POST, $_FILES)) {
-        header('Location: admin.php?page=place');
-    }
+    $result = $addPlace->addPlace($_POST, $_FILES);
+    header('Location: admin.php?page=place&error='.$result.'');
 } else if ($page == 'updatePlace') {
     $updatePlace = new Hph\Model\PlaceManager();
-    if ($updatePlace->updatePlace($_POST, $_FILES)) {
-        header('Location: admin.php?page=place');
-    }
+    $result = $updatePlace->updatePlace($_POST, $_FILES);
+    header('Location: admin.php?page=place&error='.$result.'');
 } else if ($page == 'deletePlace') {
     $deletePlace = new Hph\Model\PlaceManager();
-    if ($deletePlace->deletePlace($_POST['id'])) {
-        header('Location: admin.php?page=place');
-    }
-} else if ($page == 'news') {
+    $result = $deletePlace->deletePlace($_POST['id']);
+    header('Location: admin.php?page=place&error='.$result.'');
+}
+
+else if ($page == 'news') {
     $news = new Hph\Controller\NewsController('back');
     $render = $news->render();
 } else if ($page == 'addNews') {
     $addNews = new Hph\Model\NewsManager();
-    if ($addNews->addNews($_POST, $_FILES)) {
-        header('Location: admin.php?page=news');
-    }
+    $result = $addNews->addNews($_POST, $_FILES);
+    header('Location: admin.php?page=news&error='.$result.'');
 } else if ($page == 'updateNews') {
     $updateNews = new Hph\Model\NewsManager();
-    if ($updateNews->updateNews($_POST, $_FILES)) {
-        header('Location: admin.php?page=news');
-    }
+    $result = $updateNews->updateNews($_POST, $_FILES);
+    header('Location: admin.php?page=news&error='.$result.'');
 } else if ($page == 'deleteNews') {
     $deleteNews = new Hph\Model\NewsManager();
-    if ($deleteNews->deleteNews($_POST['id'])) {
-        header('Location: admin.php?page=news');
-    }
-} else if ($page == 'food') {
+    $result = $deleteNews->deleteNews($_POST['id']);
+    header('Location: admin.php?page=news&error='.$result.'');
+}
+
+else if ($page == 'food') {
     $food = new Hph\Controller\FoodController('back');
     $render = $food->render();
 } else if ($page == 'addFood') {
     $addFood = new Hph\Model\FoodManager();
-    if ($addFood->addFood($_POST, $_FILES)) {
-        header('Location: admin.php?page=food');
-    }
+    $result = $addFood->addFood($_POST, $_FILES);
+    header('Location: admin.php?page=food&error='.$result.'');
 } else if ($page == 'updateFood') {
     $updateFood = new Hph\Model\FoodManager();
-    if ($updateFood->updateFood($_POST, $_FILES)) {
-        header('Location: admin.php?page=food');
-    }
+    $result = $updateFood->updateFood($_POST, $_FILES);
+    header('Location: admin.php?page=food&error='.$result.'');
 } else if ($page == 'deleteFood') {
     $deleteFood = new Hph\Model\FoodManager();
-    if ($deleteFood->deleteFood($_POST['id'])) {
-        header('Location: admin.php?page=food');
-    }
-} else if ($page == 'partner') {
+    $result = $deleteFood->deleteFood($_POST['id']);
+    header('Location: admin.php?page=food&error='.$result.'');
+}
+
+else if ($page == 'partner') {
     $partner = new Hph\Controller\PartnerController('back');
     $render = $partner->render();
 } else if ($page == 'addPartner') {
     $addPartner = new Hph\Model\PartnerManager();
-    if ($addPartner->addPartner($_POST, $_FILES)) {
-        header('Location: admin.php?page=partner');
-    }
+    $result = $addPartner->addPartner($_POST, $_FILES);
+    header('Location: admin.php?page=partner&error='.$result.'');
 } else if ($page == 'deletePartner') {
     $deletePartner = new Hph\Model\PartnerManager();
-    if ($deletePartner->deletePartner($_POST['id'])) {
-        header('Location: admin.php?page=partner');
-    }
-} else if ($page == 'sunday') {
+    $result = $deletePartner->deletePartner($_POST['id']);
+    header('Location: admin.php?page=partner&error='.$result.'');
+}
+
+else if ($page == 'sunday') {
     $sunday = new Hph\Controller\DimaorlController('back');
     $render = $sunday->render();
 } else if ($page == 'addSunday') {
     $addSunday = new Hph\Model\SundayManager();
-    if ($addSunday->addSunday($_POST, $_FILES)) {
-        header('Location: admin.php?page=sunday');
-    }
+    $result = $addSunday->addSunday($_POST, $_FILES);
+    header('Location: admin.php?page=sunday&error='.$result.'');
 } else if ($page == 'updateSunday') {
     $updateSunday = new Hph\Model\SundayManager();
-    if ($updateSunday->updateSunday($_POST, $_FILES)) {
-        header('Location: admin.php?page=sunday');
-    }
+    $result = $updateSunday->updateSunday($_POST, $_FILES);
+    header('Location: admin.php?page=sunday&error='.$result.'');
 } else if ($page == 'deleteSunday') {
     $deleteSunday = new Hph\Model\SundayManager();
-    if ($deleteSunday->deleteSunday($_POST['id'])) {
-        header('Location: admin.php?page=sunday');
-    }
-} else {
+    $result = $deleteSunday->deleteSunday($_POST['id']);
+    header('Location: admin.php?page=sunday&error='.$result.'');
+}
+
+else {
     $home = new Hph\Controller\HomeController('back');
     $render = $home->render();
 }
